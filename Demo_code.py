98316cb08cd870495c4b7048209856bdc84a1955
kitzza <115458503+kitzza@users.noreply.github.com>
2022-11-13 16:06:31 +0800

    Add files via upload

---
+++ Demo_code.py
@@ -0,0 +1,27 @@
+"""
+1.九九乘法表
+2.冒泡排序
+
+
+"""
+
+
+# @1.九九乘法表
+# for i in range(1,10):
+#     for j in range(1,i+1):
+#         print(f'{j}x{i}={i*j}\t',end='')
+#     print()
+
+
+
+# # @2.冒泡排序
+# def bubble_sort(list):
+#     for i in range(0,len(list) - 1):
+#         for j in range(i + 1,len(list)):
+#             if list[i] > list[j]:
+#                 list[i],list[j] = list[j], list[i]
+#     return list
+# print(bubble_sort(list=[7,85,1,5,3,55]))
+
+
+练习

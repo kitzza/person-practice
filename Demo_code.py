0d3fcc196f9ce10600217ba400dc6be0a14e7ee4
kitzza <115458503+kitzza@users.noreply.github.com>
2022-11-13 16:55:46 +0800

    Add files via upload

--- Demo_code.py
+++ Demo_code.py
@@ -1,9 +1,16 @@
 """
 1.九九乘法表
 2.冒泡排序
-
-
+3.1~100的和
+4.分配列表变量
+5.检查内存占用
+6.交换变量
+7.组合字符串
+8.转换嵌套列表
+9.转置矩阵
+10.比较列表
 """
+
 
 
 # @1.九九乘法表
@@ -23,5 +30,58 @@
 #     return list
 # print(bubble_sort(list=[7,85,1,5,3,55]))
 
+# # 3.100内的和
+# sum(i for i in range(1,101) )  #后加print
+# # 或者 sum(range(1,101))
 
-练习
+# 4.分配列表变量
+# List = ["I","Love","Python"]
+# a, b, c = List
+# print(a,b,c )
+# #I Love Python
+# print(c,b,a )
+# #Python Love I
+
+# 5.检查内存占用
+import sys
+# a, b, c,d = "I" ,"Love", "Python", 2020
+# print(sys.getsizeof(a))
+# #50
+# print(sys.getsizeof(b))
+# #53
+# print(sys.getsizeof(c))
+# #55
+# print(sys.getsizeof(d))
+# #28
+
+# 6.交换变量
+# a,b = 'zaoqi' , 'Python'
+# a,b = b,a
+
+
+# 7.组合字符串
+# List = ['I ', 'Love ', 'Python']
+# print(''.join(List))
+# #I Love Python
+
+
+# 8.转换嵌套列表
+# import itertools
+# List = [[1, 2], [3, 4], [5, 6]]
+# print(list(itertools.chain.from_iterable(List)))
+# #[1, 2, 3, 4, 5, 6]
+
+# 9.转置矩阵
+# matrix = [[1, 2, 3], [4, 5, 6]]
+# print(list(zip(*matrix)))
+# #[(1, 4), (2, 5), (3, 6)]
+
+# 10.比较列表
+# a = ['I', 'Love', 'Python']
+# b = ['I', 'Love', 'python']
+# print(set(a).difference(set(b)))
+# print(set(a).intersection(b))
+# #{'Python'}
+# #{'Love', 'I'}
+
+
